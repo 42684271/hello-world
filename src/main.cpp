21457131bbc93e9b7e6aadb3761d147ae39778ac
42684271 <42684271@qq.com>
2019-05-30 14:38:57 +0800

    make some changes...

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,1 @@
-// main.cpp - just for test.
-
-
-void main(char** argv, unsingned int argc)
-{
-	return 0;
-}
-
-
-
-
-
-// comment 2019.03.12.. I want to have a tree..
-// change on b4 ....
+////////////////////
